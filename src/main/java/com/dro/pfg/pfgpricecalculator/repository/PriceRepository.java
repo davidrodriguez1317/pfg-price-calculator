2a2f57c6d7c3b0c647401e48ab6e20fef63408a6
davidrodriguez1317 <davidrodriguez1317@gmail.com>
2023-06-23 06:33:00 +0200

    Minor changes

--- src/main/java/com/dro/pfg/pfgpricecalculator/repository/PriceRepository.java
+++ src/main/java/com/dro/pfg/pfgpricecalculator/repository/PriceRepository.java
@@ -3,12 +3,15 @@ package com.dro.pfg.pfgpricecalculator.repository;
 import com.dro.pfg.pfgpricecalculator.model.entity.PriceEntity;
 import org.springframework.stereotype.Component;
 
+/**
+ * This class is mocking a real repository for testing purposes
+ */
 @Component
 public class PriceRepository {
     public PriceEntity getPriceByIdAndCurrency(String currency, String id) {
         return PriceEntity.builder()
                 .id(id)
-                .price(1.1f)
+                .price(56.1f)
                 .currency(currency)
                 .build();
     }
